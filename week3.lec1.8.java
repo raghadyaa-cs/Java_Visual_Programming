a96fa8eb624d7dc557b380726f7b037626e7e994
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-23 15:44:32 +0300

    Create Test8.java with greeting loop
    Add a simple Java program that prints greetings in a loop.

---
+++ week3.lec1.8.java
@@ -0,0 +1,15 @@
+package edu.zuj.vp.week3.lecture1;
+
+public class Test8 {
+    public static void main(String[] args) {
+        //three parts
+        //int i=0: initialization
+        //i<20: condition
+        //i++: operation
+        System.out.println("Hi");
+        for(int i=0;i<20;i++){
+            System.out.println("Hello "+(i+1));
+        }
+        System.out.println("Bye");
+    }
+}
