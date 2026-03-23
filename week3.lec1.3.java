b33430a2d01e2f5ad27378782cfe2506ca68b9df
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-23 15:38:43 +0300

    Add Test3 class with main method
    Fix syntax error in print statement.

---
+++ week3.lec1.3.java
@@ -0,0 +1,7 @@
+package edu.zuj.vp.week3.lecture1;
+public class Test3 {
+    public static void main(String[] args) {
+        String name="Ata";
+        System.out.println(name+(10+50));
+    }
+}
