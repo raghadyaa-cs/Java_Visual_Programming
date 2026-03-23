89d932db67d51f4873e3b87f403d0b2080df4e17
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-23 15:36:15 +0300

    Correct variable initialization in Test1 class
    Fix syntax error in variable initialization.

---
+++ week3.lec1.1.java
@@ -0,0 +1,11 @@
+package edu.zuj.vp.week3.lecture1;
+
+public class Test1 {
+ 
+    public static void main(String[] args) {
+        //int: datattype
+        //i: variable name 
+        int i=5+10;
+        System.out.println(i);
+    }
+}
