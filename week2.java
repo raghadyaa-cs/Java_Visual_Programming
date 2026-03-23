ddef5d04cb2a3061839ca7fe105e49b66a16211e
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-23 15:30:57 +0300

    Create Hello1 class with console and GUI output
    Added a simple Java application that prints a message to the console and shows a GUI dialog.

---
+++ week2.java
@@ -0,0 +1,18 @@
+package edu.zuj.demo;
+
+//any thing with { } : block
+
+import javax.swing.JOptionPane;
+
+//statment  should always end with ;: semi-column
+public class Hello1 {//left brace
+    //main: startup method for any Java application
+    public static void main(String[] args) {
+        //" : double quration           
+        //"...": String
+        //at the 
+        System.out.println("Hello from uncle Jalal");
+        JOptionPane.showMessageDialog(null, "Hello from GUI");
+    }
+    
+}//right brace
