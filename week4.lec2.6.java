7da45261c958009b8a04269e38229fde64e7bc1d
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-24 22:19:05 +0300

    Add Personv2Test class for testing PersonV2

---
+++ week4.lec2.6.java
@@ -0,0 +1,18 @@
+package edu.zuj.vp.week4.lecture2;
+
+public class Personv2Test {
+
+/*
+Pratice:
+1- Write Person v1 class and its Test
+2- Wtite Person v2 class (with encpaualtion, toString, and Constructors) 
+and its test
+3- Create first JFrame example
+
+*/
+
+    public static void main(String[] args) {
+        PersonV1 p = new PersonV2("Essa", 30);
+        p.talk();
+    }
+}
