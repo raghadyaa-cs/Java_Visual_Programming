8979e21a102dfb5bb7edb3785b00f5be1c822e54
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-24 22:25:41 +0300

    Add FirstGUI class with basic JFrame setup
    Implemented a simple JFrame example for GUI practice.

---
+++ week4.lec2.7.java
@@ -0,0 +1,15 @@
+package edu.zuj.vp.week4.lecture2;
+
+import javax.swing.JFrame;
+
+/*
+Pratice:
+3- Create first JFrame example
+*/
+public class FirstGUI {
+    public static void main(String[] args) {
+        JFrame f=new JFrame("First  GUI");
+        f.setSize(300, 300);
+        f.setVisible(true);
+    }
+}
