31675b458da6e90f4314adbafe7659d4c7382b0f
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-24 22:15:37 +0300

    Add Personv1Test class with main method

---
+++ week4.lec2.5.java
@@ -0,0 +1,18 @@
+package edu.zuj.vp.week4.lecture2;
+
+public class Personv1Test {
+
+/*
+Pratice:
+1- Write Person v1 class and its Test
+2- Wtite Person v2 class (with encpaualtion, toString, and Constructors) 
+and its test
+3- Create first JFrame example
+
+*/
+
+    public static void main(String[] args) {
+        PersonV1 p = new PersonV1("Ata", 20);
+        p.talk();
+    }
+}
