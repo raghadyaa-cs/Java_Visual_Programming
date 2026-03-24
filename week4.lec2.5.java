1f6147765ea6be8c1be9d2e55538a24320f29f23
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-03-24 22:23:48 +0300

    Update practice comment of Personv1Test class

--- week4.lec2.5.java
+++ week4.lec2.5.java
@@ -4,9 +4,9 @@ public class Personv1Test {
 
 /*
 Pratice:
-1- Write Person v1 class and its Test
+1- Write Person v1 class and its Test.
 2- Wtite Person v2 class (with encpaualtion, toString, and Constructors) 
-and its test
+and its test.
 
 */
 
