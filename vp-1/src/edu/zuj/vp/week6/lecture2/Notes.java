020b0642a65a77510a0acbed225c7263dab4774d
Raghad Yaser AbuAtwan 🪼⋆.ೃ࿔*:･ <raghadyaserabuatwan@gmail.com>
2026-05-19 13:18:28 +0300

    everything before eid adha break

---
+++ vp-1/src/edu/zuj/vp/week6/lecture2/Notes.java
@@ -0,0 +1,34 @@
+package edu.zuj.vp.week6.lecture2;
+/*
+Microsoft Word Template
+
+Requirments:
+1- Cover Page (Insert cover page)
+    - Logo
+    - Inst Name
+    - Project Name
+    - Author
+    - Advisor
+    - Date
+    - Course Number/Course Name
+2- Table of contents (References , Table of contents)
+      - We should use headings (H1, H2 , H3)
+      - Update entire table
+3- Introduction
+4- Actors
+5- Functional Requirments
+
+Tips:
+1- Date Format: April 7, 2025
+2- Images: 
+    Wrap-Text--> In front of text
+    Format - Add effect
+3- Page Numbers (Page x of Y) in the footer
+4- When you paste text, be sure to paste as "Plain Text"
+5- Use --- for draing a line
+6- Place (your-name/Project Name      Logo) in the header
+7- To Change Theme
+    Design -> Theme ->Colors
+8- Text --> Justify    
+9- Use "Dymmy Text Genertor" for getting a text
+*/
